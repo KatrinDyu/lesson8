0643b52deb3224db5c8e3c943588540a30f7a819
Ekaterina <dyuldina32@gmail.com>
2023-02-03 20:27:40 +0500

    2nd sdelano

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,5 +1,5 @@
-﻿/*  Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы
- каждой строки двумерного массива. */
+﻿/* адайте прямоугольный двумерный массив. Напишите программу, которая 
+будет находить строку с наименьшей суммой элементов. */
  Console.Clear();
  
 Console.Write("Введите количество строк массива m: ");
@@ -10,7 +10,35 @@ int columns = int.Parse(Console.ReadLine());
 
 int[,] arr = GetArray(rows, columns, 0, 10);
 PrintArray(arr);
-SortArr(arr);
+Console.WriteLine($"stroka s naimenshei summoi = {GetRow(arr)}");
+int GetRow(int[,] array)
+{
+    int row =0;
+    int minsum =0;
+    for (int i = 0; i< array.GetLength(1); i++)
+    {
+        minsum=minsum + array[0,i];
+    }
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum = sum + array[i,j];
+        }
+        if (minsum > sum)
+        {
+            minsum = sum;
+            row = i;
+        }
+    }
+    return row;
+}
+
+
+
+
+
 int[,] GetArray(int m, int n, int minValue, int maxValue)
 {
     int[,] result = new int[m, n];
@@ -32,28 +60,5 @@ void PrintArray(int[,] inArray)
             Console.Write($"{inArray[i, j]} ");
         }
         Console.WriteLine();
-    }
-}
-void SortArr(int [,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j =0; j<array.GetLength(1); j++)
-        {
-            for (int t = j+1; t<array.GetLength(1); t++)
-            {
-                if (array[i,t]>array[i,j])
-                {
-                    int temp = array[i,j];
-                    array[i,j] = array[i,t];
-                    array[i,t] = temp;
-                }
-                 
-
-            }
-Console.Write($"{array[i, j]} ");
-           
-        }
-Console.WriteLine();
     }
 }
